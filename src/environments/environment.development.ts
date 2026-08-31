ba94117137821a69c49c757841b0f086e87f5ccc
Pablo Defelitto <pdefelitto@gmail.com>
2026-08-31 11:16:09 -0300

    Conectar proyecto de Supabase y credenciales de entorno

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://iwjbjufjsotvpwctmolo.supabase.co',
+  supabaseAnonKey: 'sb_publishable_6rg9W9PIF1lunNemopbgJQ_OZgk_ErI',
 };
